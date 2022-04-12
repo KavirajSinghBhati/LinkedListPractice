6268c1eb9042c546b4c636461014aafe37b48945
Kaviraj Singh Bhati <ksb1910.07@gmail.com>
2022-04-12 22:55:06 +0530

    circular linked list insert at beginning

--- src/circularlinkedlist/CircularLinkedList.java
+++ src/circularlinkedlist/CircularLinkedList.java
@@ -17,13 +17,35 @@ public class CircularLinkedList {
         head.next.next.next = head;
 
         printList(head);
+
+        head = insertBegin(head, 5);
+        head = insertBegin(head, 2);
+
+        printList(head);
+    }
+
+    static Node insertBegin(Node head, int x) {
+        Node temp = new Node(x);
+        if (head == null) {
+            temp.next = temp;
+            return temp;
+        }
+        else {
+            temp.next = head.next;
+            head.next = temp;
+            int d = head.data;
+            head.data = temp.data;
+            temp.data = d;
+            return head;
+        }
     }
 
     static void printList(Node head) {
         Node curr = head;
         do {
-            System.out.println(curr.data);
+            System.out.print(curr.data + " ");
             curr = curr.next;
         } while (curr != head);
+        System.out.println();
     }
 }
